e491edbd0425f623aa83605e55b40c83ad4c0b73
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-16 14:34:43 +0530

    chore: added db details

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+    // db config
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = '';
+    $db_name = "todo";
+
+    // create db connection
+    $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
+
+    // check for connection error 
+    if($conn->connect_error){
+        die("connection failed :".$conn->connect_error);
+    }
