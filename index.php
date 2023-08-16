0ec2ed3075d389581b46a3831f12714b8cbf11b3
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-16 13:01:39 +0530

    chore: index file added

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>todo-app</title>
+</head>
+<body style="display: flex; align-items:center; justify-content:center; flex-direction:column;">
+    <h1 style="display: flex; ">Todo app</h1>
+    <div style="flex-direction:row;">
+    <input label="enter-todo-item"></input>
+    <button>Add</button>  
+    
+    
+</div>
+</body>
+</html>
