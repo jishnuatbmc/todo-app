9c10cd9326e9eb2799561245967e91149b2a3b99
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-17 14:42:55 +0530

    chore: auth base added

---
+++ auth/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login TODO</title>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Login</h2>
+        <form action="login/handle-login.php" method="POST" class="login-form">
+            <input name="email" placeholder="Email" />
+            <input name="password" placeholder="Password" />
+            <button>Login</button>
+        </form>
+    </div>
+</body>
+
+</html>
