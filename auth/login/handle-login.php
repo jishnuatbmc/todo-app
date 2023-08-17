802b932ac2977989a263ecff888e6a344f8fc18d
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-17 17:04:18 +0530

    updating login-handler, still error

--- auth/login/handle-login.php
+++ auth/login/handle-login.php
@@ -9,27 +9,23 @@ $password = $_POST['password'];
 // handle user login logic
 
 // queries 
-// check user
-$sql_stmt_check_user_exist = "SELECT * FROM  `users` WHERE email = ?";
-$check_user_exist = mysqli_prepare($conn, $sql_stmt_check_user_exist);
-mysqli_stmt_bind_param($check_user_exist, 's', $email);
 
 // get user 
 $sql_stmt_get_user = "SELECT * FROM `users` WHERE email = ?";
 $get_user = mysqli_prepare($conn, $sql_stmt_get_user);
 mysqli_stmt_bind_param($get_user, 's', $email);
 
+$userData = mysqli_stmt_get_result($get_user);
+$user = mysqli_fetch_array($userData);
 // case 1 : user doesnot exist
-if (!mysqli_stmt_execute($check_user_exist)) {
+if (!$user) {
     echo 'user doesnot exist, please <a href="../register.php">register</a>';
 } else {
-    mysqli_stmt_execute($get_user);
-    $user = mysqli_stmt_get_result($get_user);
-    // $user = mysqli_query($conn,$get_user);
-    echo $user['email'] . $user['password'];
-}
 
 // case 2 : user exist 
     // case 2.1 : password match : allow user 
 
     // case 2.2 : password missmatch : redirect with error message
+
+    echo $user['email'] . $user['password'];
+}
