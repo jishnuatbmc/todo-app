1f94a1ab618c4dd76ff8e2521a131b6b17de4b08
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-17 17:36:45 +0530

    register with issues

--- auth/register/handle-register.php
+++ auth/register/handle-register.php
@@ -2,20 +2,23 @@
 require_once "../../database.php";
 $email = $_POST['email'];
 $password = $_POST['password'];
-$hash_password = password_hash($password,PASSWORD_DEFAULT);
-// echo $password." and ".$hash_password;
+$hash_password = password_hash($password, PASSWORD_DEFAULT);
 
 // check for existing user?
 $sql_stmnt_check_user = "SELECT * FROM `users` WHERE email = ?;";
-$check_exist_user = mysqli_prepare($conn,$sql_stmnt_check_user);
-mysqli_stmt_bind_param($check_exist_user,'s',$email);
-if (mysqli_stmt_execute($check_exist_user)){
-    echo 'user already exist, please continue to <a href="../login.php">login</a>';
-}
-else {
-    $sql_stmnt_create_user = "INSERT INTO `users`(`email`,`password`) VALUES (?,?);";
-    $create_user = mysqli_prepare($conn, $sql_stmnt_create_user);
-    mysqli_stmt_bind_param($create_user, 'ss', $email, $hash_password);
-    mysqli_stmt_execute($create_user);
-    header("location:../login.php");
-}
+$check_exist_user = mysqli_prepare($conn, $sql_stmnt_check_user);
+mysqli_stmt_bind_param($check_exist_user, 's', $email);
+mysqli_stmt_execute($check_exist_user);
+$data = mysqli_stmt_get_result($check_exist_user);
+$user = mysqli_fetch_array($data);
+echo "user : ".$user;
+
+// if ($user) {
+//     echo 'user already exist, please continue to <a href="../login.php">login</a>';
+// } else {
+//     $sql_stmnt_create_user = "INSERT INTO `users`(`email`,`password`) VALUES (?,?);";
+//     $create_user = mysqli_prepare($conn, $sql_stmnt_create_user);
+//     mysqli_stmt_bind_param($create_user, 'ss', $email, $hash_password);
+//     mysqli_stmt_execute($create_user);
+//     header("location:../login.php");
+// }
