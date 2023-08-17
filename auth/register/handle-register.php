933035fab64d7fd1cf01640859b5a1ea03189571
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-17 16:41:10 +0530

    chore:user login error

--- auth/register/handle-register.php
+++ auth/register/handle-register.php
@@ -10,7 +10,7 @@ $sql_stmnt_check_user = "SELECT * FROM `users` WHERE email = ?;";
 $check_exist_user = mysqli_prepare($conn,$sql_stmnt_check_user);
 mysqli_stmt_bind_param($check_exist_user,'s',$email);
 if (mysqli_stmt_execute($check_exist_user)){
-    echo "user already exist, please continue to login";
+    echo 'user already exist, please continue to <a href="../login.php">login</a>';
 }
 else {
     $sql_stmnt_create_user = "INSERT INTO `users`(`email`,`password`) VALUES (?,?);";
