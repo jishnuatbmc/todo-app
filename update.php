abf705124939f6c5e128a393b448353c60e3fe39
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-18 12:14:08 +0530

    fix:issue with update and delete

--- update.php
+++ update.php
@@ -7,8 +7,15 @@ $task = $_POST["task"];
 // mysqli_query($conn,"UPDATE `todo` SET `task` = '$task' WHERE id= $id;");
 // header("location:index.php");
 
-// update-via-prepared-statement
-$update_todo = mysqli_prepare($conn,"UPDATE todo SET task = ? WHERE id = ?;");
-mysqli_stmt_bind_param($update_todo,'si',$task,$id);
-mysqli_stmt_execute($update_todo);
+// check empty string
+if (strlen($task) == 0) {
+    $delete_todo = mysqli_prepare($conn, "DELETE FROM todo WHERE id = ?");
+    mysqli_stmt_bind_param($delete_todo, 'i', $id);
+    mysqli_stmt_execute($delete_todo);
+} else {
+    // update-via-prepared-statement
+    $update_todo = mysqli_prepare($conn, "UPDATE todo SET task = ? WHERE id = ?;");
+    mysqli_stmt_bind_param($update_todo, 'si', $task, $id);
+    mysqli_stmt_execute($update_todo);
+}
 header("location:index.php");
