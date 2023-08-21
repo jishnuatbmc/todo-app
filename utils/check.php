70df5a31a367ca8f4bc70166bbf6b7be00ffa2c2
jishnuatbmc <jishnusuresh@buymeacoffee.com>
2023-08-21 14:16:33 +0530

    fix: check if user logged in complete,protect access

--- utils/check.php
+++ utils/check.php
@@ -1,4 +1,4 @@
 <?php
-if (!isset($_SESSION['user_id'])){
+if (!isset($_SESSION["user_id"]) or !$_SESSION['loggedin']) {
     header('location:/auth/login.php');
 }
--- utils/logout.php
+++ utils/logout.php
@@ -2,7 +2,12 @@
 
 function logout()
 {
-    session_destroy();
+    session_start();
+    $_SESSION["loggedin"] = false;
+    unset($_SESSION["user_id"]);
+    unset($_SESSION["email"]);
+    session_commit();
+
     header("location:/auth/login.php");
 }
 
